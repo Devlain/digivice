d6982fc5760018bfd7c231c1c0aad35f621bc391
Devlain <gonzalo_devlain@hotmail.com>
2016-05-26 11:16:36 -0300

    Update app.routes.js

--- assets/js/app.routes.js
+++ assets/js/app.routes.js
@@ -4,15 +4,15 @@
 	.config(function($routeProvider, $locationProvider) {
 		$routeProvider
 			.when('/', {
-				controller: 'MainCtrl',
 				templateUrl: 'views/main.html',
+				controller: 'MainCtrl',
 				data: {
                       private: false
                     }
 			})
 			.when('/tamerid', {
-				controller: 'TamerCtrl',
 				templateUrl: 'views/tamer.html',
+				controller: 'TamerCtrl',
 				data: {
                       private: false
                     }
