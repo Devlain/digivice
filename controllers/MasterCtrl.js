e68791d6399850a47abc752b5cfe82d06637bac3
Hernan Gonzalo Diaz Ailan <Gonzalo_Devlain@hotmail.com>
2016-05-26 15:08:05 -0300

    factories

--- controllers/MasterCtrl.js
+++ controllers/MasterCtrl.js
@@ -1,11 +1,15 @@
 (function() {
 
 	angular.module('Digivice')
-	.controller('MasterCtrl', ['$scope', MasterCtrl])
+	.controller('MasterCtrl', ['$scope', 'TamersFactory', MasterCtrl])
 
-	function MasterCtrl ($scope) {
-		
-	}
+	function MasterCtrl ($scope, TamersFactory) {
+
+		TamersFactory.success(function(data) {
+			$scope.tamers = data;
+		});
+
+	};
 	
 })();
 
---
+++ services/TamersFactory.js
@@ -0,0 +1,16 @@
+(function() {
+
+	angular.module('Digivice')
+	.factory('TamersFactory', function('$http') {
+		return
+			$http.get('assets/js/tamers.json')
+			.success(function(data) {
+				return data;
+			})
+			.error(function(err) {
+				return err;
+			});
+	});
+	
+})();
+
