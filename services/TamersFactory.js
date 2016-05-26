ea5d5b046aec3a74fc5d83728534b2992a408677
Hernan Gonzalo Diaz Ailan <Gonzalo_Devlain@hotmail.com>
2016-05-26 15:27:28 -0300

    changes

--- services/TamersFactory.js
+++ services/TamersFactory.js
@@ -2,14 +2,15 @@
 
 	angular.module('Digivice')
 	.factory('TamersFactory', ['$http', function($http) {
-		return
-			$http.get('assets/js/tamers.json')
+		var obj = '';
+		return obj
+			/*$http.get('assets/js/tamers.json')
 			.success(function(data) {
 				return data;
 			})
 			.error(function(err) {
 				return err;
-			});
+			});*/
 	}]);
 })();
 
